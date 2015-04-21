711e9880b2913a8e1343eee9e5d3c7a091adec9d
Jordan Warmoes <jordanwarmoes@gmail.com>
2015-04-21 11:58:36 +0200

    Add s to flat, redirection ok

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -14,7 +14,7 @@ class FlatsController < ApplicationController
   def create
     @flat = Flat.new(flat_params)
     @flat.save
-    redirect_to flat_path
+    redirect_to flats_path
   end
 
   def edit
@@ -24,7 +24,7 @@ class FlatsController < ApplicationController
   def update
     @flat = Flat.find(params[:id])
     @flat.update(flat_params)
-    redirect_to flat_path
+    redirect_to flats_path
   end
 
   def destroy
