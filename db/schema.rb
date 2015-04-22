7ec9bd3ecbb92f99def8a5b3904cbc3b7ee5d48e
David Bellaiche <dav.bellaiche@gmail.com>
2015-04-22 17:33:58 +0200

    Add user id to flat

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150422095423) do
+ActiveRecord::Schema.define(version: 20150422153213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,11 +41,11 @@ ActiveRecord::Schema.define(version: 20150422095423) do
     t.integer  "price"
     t.string   "title"
     t.string   "description"
-    t.integer  "user_id"
     t.string   "picture_file_name"
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
